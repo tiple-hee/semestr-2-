e1f36733945873c9003a0979f8384b4c9b079fc1
Солоницын Григорий Сергеевич ТКИ-141 <tipleeeeeeeee@gmail.com>
2026-05-24 13:45:33 +0300

    Update Peregon.h

--- Peregon.h
+++ Peregon.h
@@ -5,7 +5,7 @@
 
 /**
  * @brief Класс Перегон.
- * * Представляет собой участок пути между двумя точками (пикетами).
+ * Представляет собой участок пути между двумя точками (пикетами).
  * Может содержать внутри себя "немерные" (нестандартные) пикеты, 
  * которые влияют на общую длину перегона.
  */
@@ -18,33 +18,34 @@ public:
 
     /**
      * @brief Конструктор от двух существующих пикетов.
-     * * @param start Начальный пикет.
+     * @param start Начальный пикет.
      * @param end Конечный пикет.
      */
-    Peregon(const Piquet start,const  Piquet end);
+    Peregon(const Piquet& start, const Piquet& end);
 
     /**
      * @brief Конструктор от двух вещественных значений (координаты в метрах).
-     * * @param startMeters Начальная координата.
+     * @param startMeters Начальная координата.
      * @param endMeters Конечная координата.
      */
-    Peregon(const double startMeters,const  double endMeters);
+    Peregon(const double startMeters, const double endMeters);
+    
     /**
      * @brief Добавление нового немерного пикета на перегон.
      * Пикет добавляется в вектор и сортируется по положению (пикетажу).
-     * * @param p Объект пикета.
+     * @param p Объект пикета.
      */
     void addPiquet(const Piquet p);
 
     /**
      * @brief Расчет фактической длины перегона с учетом немерных пикетов.
-     * * @return double Длина перегона в метрах.
+     * @return double Длина перегона в метрах.
      */
     double calculateLength() const;
 
     /**
      * @brief Сериализация информации о перегоне в строку.
-     * * @return std::string Детальное описание перегона и списка его немерных пикетов.
+     * @return std::string Детальное описание перегона и списка его немерных пикетов.
      */
     std::string toString() const;
 
